0be2e7fca6c150b358a4faae5c2d425572e6baa2
nafamubarokhusni <nafamubarokhusni@gmail.com>
2026-05-28 20:29:47 +0700

    feat: add SVG icon for application branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-          <Script
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-PSW504XN9Z"
           strategy="afterInteractive"
         />
